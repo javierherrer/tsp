5bb7f6efef94b15803481260e720512f1f816f33
fuster92 <fusti92@gmail.com>
2021-05-08 10:09:53 +0200

    gtab first impl

--- TSP/src/GTab.java
+++ TSP/src/GTab.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.List;
 
 public class GTab {
@@ -7,18 +10,28 @@ public class GTab {
     // Opciones: Propuesta por JFields representación mediante bits
     //           Algo de alto nivel, con un HashSet por ejemplo.
 
+    // Guardamos la distancia desde el vertice hasta el origen con distancia K
+    // Se pueden ir borrando las distancias anteriores sin usar, K - 2 por ejemplo
+    HashMap<Vertice, HashMap<Integer, Long>> g = new HashMap<>();
+
+
     /**
      * Devuelve el coste si ha sido guardado previamente.
      * Si no, devuelve VALOR_NO_CALCULADO.
      *
      */
     public int devolverCoste (Vertice i, List<Vertice> S) {
-        //TODO
-        return VALOR_NO_CALCULADO;
+        Long coste  = g.get(i).get(S.size());
+        if (coste == null){
+
+            return VALOR_NO_CALCULADO;
+        }
+        return coste.intValue();
     }
 
+    // Es ya coste mínimo?
+    // Si coste es mínimo, se puede borrar el coste para S.size()-1
     public void guardarCoste (Vertice i, List<Vertice> S, long coste) {
-        //TODO
+        g.get(i).put(S.size(), coste);
     }
-
 }
--- TSP/src/ProgramacionDinamica.java
+++ TSP/src/ProgramacionDinamica.java
@@ -61,5 +61,4 @@ public class ProgramacionDinamica implements AlgoritmoTSP {
             }
         }
     }
-
 }
